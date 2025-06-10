cc88b98935fdfcd53c348f20801e54192a9c0a6c
samartluchstudent <samart.luch@student.passerellesnumeriques.org>
2025-06-10 10:49:35 +0700

    python

---
+++ start.py
@@ -0,0 +1,9 @@
+students=[
+{"name": "Sok", "score": 85},
+{"name": "Dara", "score": 42},
+{"name": "Rith", "score": 73},
+{"name": "Sophy", "score": 35},
+{"name": "Mony", "score": 90},
+]
+for student in students:
+    print(f"name: {student['name']}, Score: {student['score']}")
